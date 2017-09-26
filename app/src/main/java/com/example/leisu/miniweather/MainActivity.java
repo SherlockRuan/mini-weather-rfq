2f820849815ace7a2da7db7bc9666f9a72e5c154
ruanfangqi <ruanfq@pku.edu.cn>
2017-09-26 10:06:53 +0800

    init project

---
+++ app/src/main/java/com/example/leisu/miniweather/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.leisu.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by leisu on 2017/9/26.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
